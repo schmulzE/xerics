d2df5ced262e6b3af6ee48fa47810f352d228087
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-03-18 10:07:59 +0100

    implmented a reusable progressbar for displaying project progress and budget expenditure

---
+++ src/components/progressbar.jsx
@@ -0,0 +1,16 @@
+const progressbar = ({totalIncome, totalExpenses, progressValue, title}) => {
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        {totalExpenses ? <span className="text-sm capitalize">
+          <span className="text-warning">${totalExpenses}</span>/${totalIncome}</span>
+           : <span className="text-sm capitalize mt-2">{title}</span>
+        }
+        {totalExpenses ? <span className="text-xs">${totalIncome - totalExpenses} left</span> : <span className="text-xs">{progressValue}%</span>}
+      </div>
+      <progress className="progress progress-warning" value={progressValue} max="100"></progress>
+    </>
+  )
+}
+
+export default progressbar

0972f1625718cc85bb9611b1f46d35f94d211348
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-17 11:56:56 +0100

    Added utility function to extract user initials from username Implemented stringToColour function to generate consistent colors from strings Created helper functions for username initials and string-based color generation

---
+++ src/utils/stringUtils.js
@@ -0,0 +1,18 @@
+export const getUserInitials = (username) => {
+  let userInitials = username.toUpperCase().charAt(0)
+  stringToColour(username)
+  return userInitials;
+}
+
+export const stringToColour = (str) => {
+  let hash = 0;
+  str?.split('').forEach(char => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash)
+  })
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff
+    colour += value.toString(16).padStart(2, '0')
+  }
+  return colour
+}

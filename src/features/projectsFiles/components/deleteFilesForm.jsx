780f210c0f0de9a9af0da1849272ed799e7c3d5b
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-18 18:51:03 +0100

    feat: add DeleteFilesForm component for file deletion confirmation
    - Created DeleteFilesForm component to confirm file deletion action.
    - Integrated loading state with a spinner to indicate file removal in progress.
    - Implemented cancel button functionality to close the modal using dispatch from modalSlice.js.
    - Added a confirmation prompt to ensure user action before file deletion.

---
+++ src/features/projectsFiles/components/deleteFilesForm.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../modal/modalSlice.js';
+
+const DeleteFilesForm = ({removeFile, isLoading, id}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <form onSubmit={removeFile}>
+      <p className='my-4'>Are you sure you want to delete this file!</p>
+      <div className='flex justify-end gap-x-2'>
+        <button type="submit" className='btn btn-sm rounded-md flex justify-center items-center'>
+          {isLoading && <span className='pi pi-spin pi-spinner text-lg'></span>}
+          confirm
+        </button>
+        <button type="button" className='btn btn-error btn-sm rounded-md' onClick={() => dispatch(closeModal({id}))}>cancel</button>
+      </div>
+    </form>
+  )  
+}
+
+export default DeleteFilesForm;

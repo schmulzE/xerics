e735d1582e4cfc1804bdbdcf4e5e95a3a972d46a
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-18 13:39:31 +0100

    feat(auth): implement Redux slice for authentication
    Added uthSlice with initial state and reducers for managing user authentication.
    - Includes setUser and clearUser reducers for setting and clearing user state.
    - Configured extraReducers to handle async thunk actions for signIn, signUp, signOut, esetPassword, and getUser.
    - Tracks loading and error states for each action.
    - Exported selectUser selector for accessing the current user from the state.
    
    This setup centralizes authentication state management and integrates with Redux Toolkit thunks.

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,76 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { signIn, signUp, signOut, resetPassword, getUser } from './authThunks';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = null;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(signIn.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signIn.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(signIn.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(signUp.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(signOut.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(signOut.fulfilled, (state) => {
+        state.loading = false;
+        state.user = null;
+      })
+      .addCase(signOut.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(resetPassword.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(resetPassword.fulfilled, (state) => {
+        state.loading = false;
+        state.user = null;
+      })
+      .addCase(resetPassword.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { setUser, clearUser} = authSlice.actions;
+export const selectUser = (state) => state.auth.user;
+export default authSlice.reducer;

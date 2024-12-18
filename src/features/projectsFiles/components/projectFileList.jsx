cc0d9744bf7c47a71379b85595009b13db9b9cc4
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-18 19:05:35 +0100

    feat: implement FileList component for displaying and managing files
    - Added FileList component to display a list of files with their name, size, and appropriate icon based on file type.
    - Includes file selection functionality with checkboxes to allow multiple file selections.
    - Supports dynamic button rendering via uttons prop for actions like file deletion.
    - Implements ytesToSize helper function to format file sizes in KB or MB.
    - Uses handleIconBasedOnFileType to display different icons based on file MIME types, supporting images, Word, PDF, and Excel files.

---
+++ src/features/projectsFiles/components/projectFileList.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable no-unused-vars */
+import image from '@/assets/images/image.png';
+import word from '@/assets/images/word.png';
+import pdf from '@/assets/images/pdf.png';
+import excel from '@/assets/images/excel.png';
+import text from '@/assets/images/txt.png';
+import FileDeleteButton from '../../projectsFiles/components/fileDeleteButton';
+
+         
+const FileList = ({ files, buttons, selectedFiles, setSelectedFiles, loading}) => {                
+  
+  function bytesToSize(bytes) {
+    const kilobytes = bytes / 1024;
+    const megabytes = kilobytes / 1024;
+    return megabytes >= 1 ? megabytes.toFixed(2) + " MB" : kilobytes.toFixed(2) + " KB";
+  }
+  
+
+  function handleIconBasedOnFileType(fileType) {
+    let imageSource;
+     // Determine the image source based on the file type
+     switch (fileType) {
+      case 'image/jpg':
+      case 'image/jpeg':
+      case 'image/png':
+        imageSource = image;
+        break;
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        imageSource = word;
+        break;
+      case 'application/pdf':
+        imageSource = pdf;
+        break;
+      case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+        imageSource = excel;
+        break;
+      default:
+        // Provide a default image source if the file type is not recognized
+       imageSource = text;
+    }
+    return imageSource;
+  }
+
+
+  const selectFiles = (e) => {
+    const { value } = e.target;
+    const newSelectedValues = selectedFiles.includes(value)
+      ? selectedFiles.filter((val) => val !== value)
+      : [...selectedFiles, value];
+    setSelectedFiles(newSelectedValues);
+  }
+  
+  // if(files == null || loading) return <div>loading...</div>
+
+  return (
+    <ul className='flex gap-2'>
+      {files && files.map((file) => (
+        <li 
+        className="bg-base-100 flex justify-between items-center rounded-md border border-base-300 p-2 w-80" 
+        key={file.lastModified || file.id}
+        >
+          <input
+          name='checkbox'
+          value={file.id} 
+          type="checkbox" 
+          checked={selectedFiles.includes(file.id)}
+          onChange={selectFiles} 
+          className='cursor-pointer'
+          />
+          <div className='flex items-center gap-x-1'>
+            <img src={handleIconBasedOnFileType(file.type)} alt='image of file type' className='w-10 h-10 m-1'/>
+            <div className='flex flex-col gap-y-1 text-center'>
+              <span className="text-xs font-medium ">{file.name}</span>
+              <span className="text-xs text-gray-600">{ bytesToSize(file.size) }</span>
+            </div>
+          </div>
+          <div>
+          {buttons?.map((button, index) => (
+            <FileDeleteButton button={button} file={file} key={index}/>
+          ))}
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default FileList

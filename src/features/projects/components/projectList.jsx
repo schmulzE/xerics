84f36ee16ac8d79f343375989a898c6902f65202
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-18 16:49:40 +0100

    feat(ProjectList): integrate boards data and dynamic progress calculation
    - Added etchBoards dispatch in useEffect to load boards data from the Redux store.
    - Used useSelector to access boards and calculate task completion percentages dynamically.
    - Implemented calculateProgressValue to determine project progress based on completed tasks in the 'done' board.
    - Passed dynamic progressValue to each ProjectCard for accurate progress display.
    - Optimized rendering of project cards with conditional checks and structured props.

---
+++ src/features/projects/components/projectList.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import ProjectCard from "./projectCard.jsx";
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchBoards } from "../../boards/boardsSlice.js";
+
+const ProjectList = ({projects}) => {
+
+  const dispatch = useDispatch();
+  const boards = useSelector(state => state.board.boards);
+  
+  useEffect(() => {
+    dispatch(fetchBoards());
+  }, [dispatch]);
+
+
+  const calculateProgressValue = (project) => {
+    const board = boards.find(board => board.title === 'done');
+    const completedTasks = board ? project.tasks.filter(task => task.board_id === board.id) : [];
+    const totalTasks = project.tasks.length;
+    return totalTasks > 0 ? Math.floor((completedTasks.length / totalTasks) * 100) : 0;
+  };
+  
+
+  return (
+    <ul className='flex flex-col lg:flex-row w-full gap-8 mt-16'>
+      {projects.map((project) => ( project &&
+        <li className="w-80" key={project.id}>
+        <ProjectCard
+          id={project.id}
+          name={project.name} 
+          imagePath={project.image}
+          status={project.status}
+          classes={'rounded-sm h-full'}
+          description={project.description}
+          statusClass={''}
+          icon="pi-ellipsis-v"
+          progressValue={calculateProgressValue(project)}
+        />
+        </li>
+      ))}  
+    </ul>
+  )
+}
+
+export default ProjectList

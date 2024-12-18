1516ea4a809a421400d42ff3ea6a330792c5fb51
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-18 14:30:05 +0100

    feat(ProjectProgressChart): add pie chart to visualize project status
    - Implemented a Pie chart using Chart.js to show the distribution of projects by their current status: In Progress, Done, and In Review.
    - The chart dynamically updates based on the projectsInProgress, inishedProjects, and projectsUnfinished props.
    - Styled the chart with distinct colors for each project status: orange for 'In Progress', green for 'Done', and red for 'In Review'.
    - Added fallback handling to display a greyed-out chart when no project data is available.
    - Included a legend and data labels to provide project count information for each status.
    - Wrapped the chart in a CardWrapper component for consistent styling and layout.

---
+++ src/features/projects/charts/projectProgressChart.jsx
@@ -0,0 +1,94 @@
+import { Pie } from 'react-chartjs-2';
+import CardWrapper from '../../../components/ui/cardWrapper';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
+export function ProjectProgressChart({projectsUnfinished, projectsInProgress, finishedProjects}) {
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom',
+        display: false
+      },
+    },
+  };
+  
+  const data = {
+    labels: ['In Progress', 'Done', 'In Review'],
+    datasets: [
+      {
+        label: '# of Projects',
+        data: [ projectsInProgress, finishedProjects, projectsUnfinished],
+        backgroundColor: [
+         'rgb(248,102,36)',
+         'rgb(34,202,173)',
+         'rgb(236,72,87)',
+        ],
+        borderColor: [
+          'rgb(248,102,36)',
+          'rgb(34,202,173)',
+          'rgb(236,72,87)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const Emptydataset = {
+    labels: ['In Progress', 'Done', 'In Review'],
+    datasets: [
+      {
+        label: '# of Projects',
+        data: ['1'],
+        backgroundColor: [
+         'rgba(128,128, 128, 0.5)',
+        ],
+        borderColor: [
+          'rgba(128,128,128, 0.1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <CardWrapper
+      containerClass={'bg-base-100 p-4 lg:w-2/5 w-full lg:mt-8 capitalize'}
+      title='all project'
+      subtitle='based on status'
+      subtitleClass={'text-xs'}
+    >
+      <div className='h-64 flex justify-center'>
+        <Pie data={!projectsUnfinished || !projectsInProgress || !finishedProjects ? Emptydataset : data} options={options}/>
+      </div>
+      <ul className='flex flex-col text-sm justify-center space-y-2 mt-2'>
+        <li className='flex justify-around'>
+          <div>
+            <span className='inline-block w-4 h-4 bg-[rgb(248,102,36)] rounded-full mr-2'></span>
+            <span>In progress</span>
+          </div>
+          <div className='font-bold'>{projectsInProgress}</div>
+        </li>
+        <li className='flex justify-around gap-x-9'>
+          <div>
+            <span className='inline-block w-4 h-4 bg-[rgb(34,202,173)] rounded-full mr-2'></span>
+            <span>Done</span>
+          </div>
+          <div className='font-bold'>{projectsInProgress}</div>
+        </li>
+        <li className='flex justify-around gap-x-2'>
+          <div>
+            <span className='inline-block w-4 h-4 bg-[rgb(236,72,87)] rounded-full mr-2'></span>
+            <span>In Review</span>
+          </div>
+          <div className='font-bold'>{finishedProjects}</div>
+        </li>
+      </ul>
+    </CardWrapper>
+    )
+  ;
+}

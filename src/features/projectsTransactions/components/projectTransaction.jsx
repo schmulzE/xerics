74eecb6229931d7445768d7cd97191e94ff137e5
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-19 23:01:58 +0100

    feat: add ProjectTransaction component for displaying transactions
    - Created ProjectTransaction component to display a list of project transactions.
    - Differentiates transactions by type (Income or Expenses) with distinct icons and styles.
    - Supports light and dark themes via 	hemeContext.
    - Displays a placeholder message when no transactions are available.

---
+++ src/features/projectsTransactions/components/projectTransaction.jsx
@@ -0,0 +1,27 @@
+import { useTheme } from '../../../../context/themeContext';
+
+const ProjectTransaction = ({transactions}) => {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <h1 className="my-2">Transactions</h1>
+      <ul className="rounded-box space-y-4">
+        {transactions.length > 0 ? transactions.map((transaction) => (
+          <li className="flex justify-between items-center" key={transaction.id}>
+          <div className={transaction.type == "Income" ? `bg-green-100 rounded-full ${theme === 'dark' ? 'dark:bg-[#191E24]' : null} ` : ` ${theme === 'dark' ? 'dark:bg-[#191E24]' : 'bg-red-50'} rounded-full`}>
+            <i className={transaction.type == "Income" ? "text-green-600 p-2 pi pi-credit-card text-sm" : " p-2 pi pi-credit-card text-red-600 text-sm"}></i>
+          </div>
+          <div className="flex flex-col text-sm text-center">
+            <span className="font-medium">{transaction.transaction}</span>
+            <span className="text-xs">{transaction.type}</span>
+          </div>
+          <span className="text-xs">{transaction.type == "Expenses" ? `-$${transaction.amount}` : `$${transaction.amount}`}</span>
+        </li>
+        )) : <div className='capitalize flex flex-col h-44 justify-center content-center text-sm font-normal'><p className='m-auto'>No transactions made</p></div> }
+      </ul>
+    </>
+  )
+}
+
+export default ProjectTransaction

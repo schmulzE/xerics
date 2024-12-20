abc10a8c84908a060c314ffbecbb8e428b59b5e0
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-20 13:23:27 +0100

    feat(ui): Implement SearchInputWithIcon component
    This commit introduces a reusable SearchInputWithIcon component
    for building search bars in the UI. It includes:
    
    - A styled search bar with a border and background color
    - A search icon using PrimeIcons (pi pi-search)
    - An input field with placeholder text and value binding
    - Trimming leading whitespace on input change
    
    Provides a cleaner and more consistent way to implement search functionality.

---
+++ src/components/ui/searchInput.jsx
@@ -0,0 +1,16 @@
+const SearchInputWithIcon = ({query, setQuery, placeholder}) => {
+  return (
+    <div className='bg-base-100 flex border border-gray-300 items-center p-1'>
+      <i className='pi pi-search align-middle items-baseline text-gray-400 p-2'></i>
+      <input 
+      className='outline-none text-sm bg-base-100' 
+      placeholder={placeholder} 
+      id="search-project"
+      value={query}
+      onChange={(e) => setQuery(e.target.value.trimStart())}
+      />
+    </div>
+  )
+}
+
+export default SearchInputWithIcon;

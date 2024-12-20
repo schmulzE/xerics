181003537d81d0c549c7a726176a3fc0b3c459c8
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-20 22:47:28 +0100

    feat(ui): Implement CardWrapper component
    This commit introduces a reusable CardWrapper component for displaying card-like content. It includes:
    
    - Supports customization through props for title, subtitle, text, container styling, and button placement.
    - Renders a title, subtitle, and large text within the card.
    - Accepts children for additional content inside the card body.
    - Includes a button area with FileDeleteButton components if a uttons prop is provided.
    
    Provides a flexible layout for presenting various content types within a card structure.

---
+++ src/components/ui/cardWrapper.jsx
@@ -0,0 +1,35 @@
+import FileDeleteButton from "../../features/projectsFiles/components/fileDeleteButton"
+
+const CardWrapper = ({
+    title,
+    titleClass,
+    subtitle,
+    subtitleClass,
+    text,
+    textClass,
+    containerClass,
+    buttonWrapper,
+    buttons,
+    children
+  }) => {
+  return (
+    <div className={`${containerClass} rounded-lg bg-base-100 p-4`}>
+      <div className="flex justify-between items-center">
+        <div className="">
+          <h1 className={`text-lg font-medium ${titleClass}`}>{title}</h1>
+          <h2 className={`${subtitleClass}`}>{subtitle}</h2>
+        </div>
+        <div className={`${buttonWrapper}`}>
+        { buttons && buttons.map((button, index) => (
+          <FileDeleteButton key={index} button={button} file={{}}/>
+          ))
+        }
+        </div>
+      </div>
+      <h1 className={`text-4xl font-semibold my-2 ${textClass}`}>{text}</h1>
+      {children}
+    </div>
+  )
+}
+
+export default CardWrapper

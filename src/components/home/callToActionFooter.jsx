42cf6cc229d8f7d7c7909f29041f0e6cf55bb375
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-12-20 11:09:34 +0100

    feat: Implemented CallToActionFooter component
    This commit adds a CallToActionFooter component to provide a final call to action at the bottom of the page. It includes:
    
    - A clear and concise message: 'Ready to transform your projects? Get Started Today!'
    - A prominent call-to-action button: 'Get Started' with an arrow icon.
    - Distinct styling with a blue background and white text for high visibility.

---
+++ src/components/home/callToActionFooter.jsx
@@ -0,0 +1,11 @@
+
+const CallToActionFooter = () => {
+  return (
+    <div className="bg-blue-600 text-base-100 text-center lg:py-14 py-14 space-y-4">
+      <p className='text-2xl lg:text-3xl font-medium'>Ready to transform your projects? Get Started Today!</p>
+      <button className="btn btn-md btn-white text-blue-500 capitalize">Get Started <i className="pi pi-arrow-right"></i></button>
+    </div>
+  )
+}
+
+export default CallToActionFooter;

7c5eb9db15a85600c1d19946cbc2a1bdd99ef954
wisdom eshiet <wisdomeshiet65@gmail.com>
2025-01-05 09:08:31 +0100

    refactor: changed the height of the banner image via tailwind css

--- src/components/home/heroSection.jsx
+++ src/components/home/heroSection.jsx
@@ -1,4 +1,5 @@
-import xericsDashboard from '@/assets/images/xericsDashboard.png';
+import xericsDashboard from '@/assets/images/dashboard.png';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
@@ -6,9 +7,9 @@ const HeroSection = () => {
       <div className="lg:text-6xl text-3xl font-medium text-black md:font-medium capitalize lg:mb-4">unlock your team&apos;s</div>
       <div className="lg:text-6xl text-3xl font-medium text-black capitalize">productivity with <span className="text-blue-600">xerics management</span></div>
       <p className="lg:my-12 lg:text-base my-8 text-sm md:px-28  lg:px-56">Streamline your workflow, enhance team collaboration, and achieve your goals effortlessly</p>
-      <button className="btn btn-base bg-blue-500 hover:bg-blue-700 text-base-100 capitalize">Get Started <i className="pi pi-arrow-right"></i></button>
+      <Link to='/signin' className="btn btn-base bg-blue-500 hover:bg-blue-700 text-base-100 capitalize">Get Started <i className="pi pi-arrow-right"></i></Link>
       <div 
-        className="lg:mx-auto lg:w-3/4 md:left-28 lg:mt-14 w-full lg:h-[500px] h-64 bg-contain bg-no-repeat lg:bg-cover bg-center rounded-lg" 
+        className="lg:mx-auto mt-8 lg:w-3/4 md:left-28 lg:mt-14 w-full lg:h-[700px] h-64 bg-contain bg-no-repeat lg:bg-contain lg:border bg-center rounded-lg" 
         style={{ backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 1)), url(${xericsDashboard})` }} 
       />
       <div className="flex flex-wrap content-center justify-center gap-3 mt-8 lg:mt-28 lg:w-[480px] lg:mx-auto">

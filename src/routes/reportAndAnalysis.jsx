91fcbdb2b160dc1ffdc37c5edf51f79b9fa7311b
wisdom eshiet <wisdomeshiet65@gmail.com>
2025-06-06 12:27:48 +0100

    refactor: improve variable naming and structure in ReportAndAnalysis
    Refactored ReportAndAnalysis component by renaming variables for clarity (e.g., BreadcrumbLinks, Projects) and organizing code for better readability and maintainability.

--- src/routes/reportAndAnalysis.jsx
+++ src/routes/reportAndAnalysis.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react"; 
-import ProjectTable from '../features/projects/components/projectTable';
+import Breadcrumbs from '@/components/ui/breadcrumbs';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProjects } from '../features/projects/projectThunks';
+import ProjectTable from '../features/projects/components/projectTable';
 import ProjectBarChart from '../features/projects/charts/projectBarChart';
 import ProjectTasksStat from "../features/projects/charts/projectTasksStat";
 import PriorityLevelStat from '../features/projects/charts/priorityLevelStat';
@@ -14,9 +15,23 @@ const ReportAndAnalysis = () => {
     dispatch(fetchProjects());
   }, [dispatch]);
 
+  const BreadcrumbLinks = [
+    {
+      text: 'Dashboard',
+      url: '/dashboard',
+    },
+    {
+      text: 'Report and Analysis',
+      url: '/report-and-analysis',
+    }
+  ]
+
   return (
-    <div className={`px-4 pb-32 lg:pb-0 lg:p-8 space-y-4 lg:space-y-8`}>
-      <h1 className='text-2xl font-medium first-letter:uppercase'>report and analysis</h1> 
+    <div className={`px-4 pb-32 lg:pb-0 lg:px-8 lg:py-2 space-y-4 lg:space-y-8`}>
+      <div>
+        <h1 className='text-2xl font-semibold lg:text-3xl lg:font-bold first-letter:uppercase'>report and analysis</h1> 
+        <Breadcrumbs links={BreadcrumbLinks}/>
+      </div>
       <ProjectBarChart projects={Projects}/>
       <div className="flex justify-between flex-col gap-y-4 lg:gap-y-0 lg:flex-row">
         <ProjectTasksStat/>
